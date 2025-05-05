b70e04204f0316479ef6602072cb3618954f07ab
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-05-05 23:56:57 +0700

    feat: add CarvedRock Models using database first

---
+++ EFCoreLearning.DataAccess/Models/AddressCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreLearning.DataAccess.Models;
+
+public partial class AddressCustomer
+{
+    public int CustomerId { get; set; }
+
+    public string AddressId { get; set; } = null!;
+
+    public int? Id { get; set; }
+
+    public virtual Address Address { get; set; } = null!;
+
+    public virtual Customer Customer { get; set; } = null!;
+}
---
+++ EFCoreLearning.DataAccess/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreLearning.DataAccess.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public int? Age { get; set; }
+
+    public int CustomerRankId { get; set; }
+
+    public virtual CustomerRank CustomerRank { get; set; } = null!;
+}
