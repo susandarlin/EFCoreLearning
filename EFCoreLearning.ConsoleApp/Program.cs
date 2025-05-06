9ec38e35ea222dbf93c0df53e4b08ec680f16574
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-05-06 23:56:38 +0700

    feat: add DataAccess project as project dependency in ConsoleApp project

--- EFCoreLearning.ConsoleApp/Program.cs
+++ EFCoreLearning.ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿    // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Press any key to exit");
+Console.Read();
